1e3551c30d29a196e9e07aee111cf9026864f40d
Scott Chamberlain <myrmecocystus@gmail.com>
2015-02-16 11:31:47 -0800

    added pkg level man file

---
+++ R/binomen-package.R
@@ -0,0 +1,8 @@
+#' Taxonomic class specification and parsing methods
+#'
+#' @name binomen-package
+#' @aliases binomen
+#' @docType package
+#' @title Taxonomic class specification and parsing methods
+#' @keywords package
+NULL
