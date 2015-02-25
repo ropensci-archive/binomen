35916504556707df043eeadf71a5afae6311a5a1
Scott Chamberlain <myrmecocystus@gmail.com>
2015-02-25 15:07:12 -0800

    added .taxa method to pop function

--- R/pop.R
+++ R/pop.R
@@ -5,17 +5,18 @@
 #'
 #' @param .data Input, object of class taxon
 #' @param ... Further unnamed args, see examples
-#' @return For \code{taxon} inputs gives back a \code{taxon} object. For \code{taxondf}
-#' inputs, gives back a \code{taxondf} object.
+#' @return For \code{taxon} inputs gives back a \code{taxon} object. For \code{taxa} inputs
+#' gives back a \code{taxa} object. For \code{taxondf} inputs, gives back a \code{taxondf}
+#' object.
 #' @examples
 #' # operating on `taxon` objects
 #' out <- make_taxon(genus="Poa", epithet="annua", authority="L.",
 #'    family='Poaceae', clazz='Poales', kingdom='Plantae', variety='annua')
-#' ## single names
+#' ## single taxonomic group
 #' out %>% pop(family)
 #' out %>% pop(genus)
 #' out %>% pop(species)
-#' ## many names
+#' ## many taxonomic groups
 #' out %>% pop(family, genus, species)
 #'
 #' # operating on taxonomic data.frames
@@ -27,10 +28,19 @@
 #'          stringsAsFactors = FALSE)
 #' (df2 <- taxon_df(df))
 #'
-#' ## pop out a single taxonomic class
+#' ## pop out a single taxonomic group
 #' df2 %>% pop(order)
 #' df2 %>% pop(family)
 #' df2 %>% pop(genus)
+#'
+#' ## pop out many taxonomic groups
+#' df2 %>% pop(order, family)
+#' df2 %>% pop(order, genus)
+#'
+#' # From taxa object
+#' df2 %>% scatter %>% pop(family)
+#' df2 %>% scatter %>% pop(family, species)
+#' df2 %>% scatter %>% pop(family, species, genus)
 pop <- function(.data, ...) {
   UseMethod("pop")
 }
@@ -44,8 +54,13 @@ pop.taxon <- function(.data, ...){
 }
 
 #' @export
+pop.taxa <- function(.data, ...){
+  taxa(lapply(.data, pop, ...))
+}
+
+#' @export
 pop.taxondf <- function(.data, ...){
   var <- vars(...)
   check_vars(var, names(.data))
-  select_(.data, paste0("-", var, collapse = ""))
+  select_(.data, .dots = paste("-", var, sep = ""))
 }
